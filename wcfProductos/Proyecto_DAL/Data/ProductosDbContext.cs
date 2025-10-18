fc90dc0af7b624e16254e79b1e3ae89b8ec4e778
Tito Calleros <tito.calleros@gmail.com>
2025-10-18 16:31:52 -0600

    Crear el Proyecto_DAL para acceso a datos
    Agregar la configuración a la BD en el archivo de configuración
    Crear la clase del DbContext

---
+++ wcfProductos/Proyecto_DAL/Data/ProductosDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto_ENT.Entidades;
+
+namespace Proyecto_DAL.Data
+{
+    public class ProductosDbContext : DbContext
+    {
+
+        public ProductosDbContext()
+        {            
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
