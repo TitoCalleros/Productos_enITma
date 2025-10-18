e39f687b4190a8d8d71f87ac86e37f89dbc8119d
Tito Calleros <tito.calleros@gmail.com>
2025-10-18 16:41:24 -0600

    Agregar la interfaz para el Repositorio

---
+++ wcfProductos/Proyecto_ENT/Interfaces/IRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Proyecto_ENT.Interfaces
+{
+    public interface IRepositorio<T> where T : class
+    {
+        IEnumerable<T> ObtenerTodos();
+        T ObtenerPorId(int id);
+        void Agregar(T entidad);
+        void Actualizar(T entidad);
+        void Eliminar(int id);
+
+        IEnumerable<T> GetPaged<TKey>(int pageNumber, int pageSize);
+    }
+}
