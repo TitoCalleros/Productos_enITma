825a9c4880564338d1e20269452fdabd0914fb1c
Tito Calleros <tito.calleros@gmail.com>
2025-10-18 16:05:50 -0600

    Crear solución y Proyecto_ENT con la entidad Producto
    Creaf de la solución wcfProductos.
    
    Crear Proyecto_ENT con la entidad Producto.

---
+++ wcfProductos/Proyecto_ENT/Producto.cs
@@ -0,0 +1,9 @@
+﻿namespace Proyecto_ENT
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }        
+        public string Descripcion { get; set; }
+        public int IdUsuario { get; set; }        
+    }
+}
