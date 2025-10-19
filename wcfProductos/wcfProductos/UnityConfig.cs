a69ef519094d4172a00dc4b8526d698c69284b29
Tito Calleros <tito.calleros@gmail.com>
2025-10-19 11:20:23 -0600

    Implementar Inyección de Dependencias con Unity

---
+++ wcfProductos/wcfProductos/UnityConfig.cs
@@ -0,0 +1,48 @@
+﻿using Unity;
+using Unity.Lifetime;
+using Proyecto_BLL.Interfaces;
+using Proyecto_BLL.Servicios;
+using Proyecto_DAL.Data;
+using Proyecto_DAL.Data.Repositorios;
+using Proyecto_ENT.Entidades;
+using Proyecto_ENT.Interfaces;
+using Proyecto_BLL.Mappings;
+using AutoMapper;
+
+namespace wcfProductos
+{
+    public static class UnityConfig
+    {
+        private static IUnityContainer _container;
+
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            if (_container == null)
+            {
+                _container = new UnityContainer();
+                _container.AddExtension(new Diagnostic());
+
+                // EntityFramework
+                _container.RegisterType<ProductosDbContext>(new HierarchicalLifetimeManager());
+
+                // Repositorio
+                _container.RegisterType<IRepositorio<Exa_CatalogoProducto>, Repositorio<Exa_CatalogoProducto>>();
+
+                // AutoMapper
+                var mapperConfig = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile<ProductoProfile>();
+                });
+
+                _container.RegisterInstance<IMapper>(mapperConfig.CreateMapper());
+
+                // Servicios
+                _container.RegisterType<IProductosService, ProductoService>();
+
+               _container.RegisterType<IProductoWcf, ProductoWcf>();
+
+            }
+            return _container;
+        }
+    }
+}
---
+++ wcfProductos/wcfProductos/UnityServiceHostFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using Unity;
+using Unity.Wcf;
+
+namespace wcfProductos
+{
+
+    public class UnityServiceHostFactory : Unity.Wcf.UnityServiceHostFactory
+    {
+        protected override void ConfigureContainer(IUnityContainer container)
+        {
+            UnityConfig.GetConfiguredContainer();
+        }
+
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            var container = UnityConfig.GetConfiguredContainer();
+            return new UnityServiceHost(container, serviceType, baseAddresses);
+        }
+    }
+}
