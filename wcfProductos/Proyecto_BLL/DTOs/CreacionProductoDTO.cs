7c6291c98f4d3bbd8eafc3b1763bdcaaebbe8004
Tito Calleros <tito.calleros@gmail.com>
2025-10-18 18:29:40 -0600

    Agregar el Proyecto_BLL y los DTOs
    Agregar los DTOs CreacionProductoDTO y ProductoDTO

---
+++ wcfProductos/Proyecto_BLL/DTOs/CreacionProductoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_BLL.DTOs
+{
+    [DataContract]
+    public class CreacionProductoDTO
+    {
+        [DataMember]
+        public string Descripcion { get; set; }
+
+        [DataMember]
+        public int IdUsuario { get; set; }        
+    }
+}
---
+++ wcfProductos/Proyecto_BLL/DTOs/ProductoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_BLL.DTOs
+{
+    [DataContract]
+    public class ProductoDTO
+    {
+        [DataMember]
+        public int IdProducto { get; set; }        
+        [DataMember]
+        public string Descripcion { get; set; }                
+    }
+}
