4f43c0dae85f8e78c3856cf5f08ce7edbae3718c
Tito Calleros <tito.calleros@gmail.com>
2025-10-19 11:22:22 -0600

    Configurar el servicio ProductoWcf

---
+++ wcfProductos/wcfProductos/IProductoWcf.cs
@@ -0,0 +1,22 @@
+﻿using Proyecto_BLL.DTOs;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace wcfProductos
+{
+    
+    [ServiceContract]
+    public interface IProductoWcf
+    {
+
+        [OperationContract]
+        Task<List<ProductoDTO>> RecuperarCatalogo();
+
+        [OperationContract]
+        Task<int> AgregarProducto(CreacionProductoDTO productoDTO);
+    }
+
+
+    
+}
---
+++ wcfProductos/wcfProductos/ProductoWcf.svc.cs
@@ -0,0 +1,32 @@
+﻿using Proyecto_BLL.DTOs;
+using Proyecto_BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Unity;
+
+namespace wcfProductos
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class ProductoWcf : IProductoWcf
+    {
+        private readonly IProductosService _productosService;
+
+        [InjectionConstructor]
+        public ProductoWcf(IProductosService productosService)
+        {
+            _productosService = productosService;
+        }
+
+        public async Task<int> AgregarProducto(CreacionProductoDTO productoDTO)
+        {
+            return await _productosService.CrearProductoAsync(productoDTO);
+        }
+
+        public async Task<List<ProductoDTO>> RecuperarCatalogo()
+        {
+            return await _productosService.ObtenerProductosAsync();
+        }
+    }
+}
