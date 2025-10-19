b93aedf60e679b031fba6f7c2638db3db2d211bb
Tito Calleros <tito.calleros@gmail.com>
2025-10-19 16:28:07 -0600

    Agregar el esquema y la tabla a la que mapear la entidad Exa_CatalogoProducto

--- wcfProductos/Proyecto_ENT/Entidades/Exa_CatalogoProducto.cs
+++ wcfProductos/Proyecto_ENT/Entidades/Exa_CatalogoProducto.cs
@@ -1,12 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Proyecto_ENT.Entidades
 {
+    [Table("Exa_CatalogoProducto", Schema = "Ventas")]
     public class Exa_CatalogoProducto
     {
         [Key]
         public int IdProducto { get; set; }        
         public string Descripcion { get; set; }
-        public int IdUsuario { get; set; }        
+        public int IdUsuario { get; set; }
     }
 }
